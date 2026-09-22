eaa76791d534ac59d2d19d5152fbc165ba92fad7
keen-coder <kknaur@gmail.com>
2026-09-22 12:34:37 -0400

    Updated Week 06 Examples

--- week06_object_oriented_programming_ii/deep_copying_classes/book.py
+++ week06_object_oriented_programming_ii/deep_copying_classes/book.py
@@ -1,59 +1,16 @@
 class Book:
-    """
-    Represents a book in a library.
-
-    A Book object stores information about a book's
-    title and author. This class is intentionally
-    mutable because the title can be changed after
-    the object is created.
-    """
-
     def __init__(self, title: str, author: str):
-        """
-        Initializes a Book object.
-
-        Args:
-            title: The title of the book.
-            author: The name of the author.
-        """
         self.__title = title
         self.__author = author
 
     def get_title(self) -> str:
-        """
-        Returns the book's title.
-
-        Returns:
-            The title of the book.
-        """
         return self.__title
 
     def get_author(self) -> str:
-        """
-        Returns the book's author.
-
-        Returns:
-            The author's name.
-        """
         return self.__author
 
     def set_title(self, title: str) -> None:
-        """
-        Updates the book's title.
-
-        Args:
-            title: The new title.
-        """
         self.__title = title
 
     def __str__(self) -> str:
-        """
-        Returns a string representation of the book.
-
-        Returns:
-            A string containing the title and author.
-        """
         return f'"{self.__title}" by {self.__author}'
-
-    def __repr__(self) -> str:
-        return self.__str__()
--- week06_object_oriented_programming_ii/deep_copying_classes/library.py
+++ week06_object_oriented_programming_ii/deep_copying_classes/library.py
@@ -2,10 +2,6 @@ from book import Book
 from copy import deepcopy
 
 class Library:
-    """
-    Represents a library.
-    """
-
     def __init__(self, name: str, books: list[Book]):
         self.__name = name
         self.__books = books
@@ -23,4 +19,4 @@ class Library:
        return deepcopy(self)
 
     def __str__(self):
-        return f'Library: {self.__name}, {self.__books}'
+        return f'Library: {self.__name}, {', '.join(self.__books)}'
